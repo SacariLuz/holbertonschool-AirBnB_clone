6077a524102bbb918f2e47e3e1f9c4fa57cddbd3
root <asacaricachi@gmail.com>
2023-10-24 21:11:26 -0700

    defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ import datetime
 class BaseModel:
 
     """Esta clase represent BaseModel"""
+
     def __init__(self):
         """metodo que inicialize"""
         self.id = str(uuid.uuid4())
@@ -19,21 +20,21 @@ class BaseModel:
         """
         devuelve una representacion de nuestra instancia
         """
-        return ("[{}] ({}) {}"
-                .format(self.__class__.__name__, self.id, self.__dict__))
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
 
     def save(self):
         """metodo que actualiza la fecha de creación updated_at."""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
         metodo que devuelve un diccionario
+
         Returns:
         dict:devuelve un diccionario con los atributos de instancia.
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        ttributes = self.__dict__.copy()
+        attributes['__class__'] = self.__class__.__name__
+        attributes['created_at'] = self.created_at.isoformat()
+        attributes['updated_at'] = self.updated_at.isoformat()
+        return attributes
