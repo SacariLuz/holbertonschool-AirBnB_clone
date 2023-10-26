3860cb77ea6eaa64ad3b7a916ff6a73ea52f2c64
root <asacaricachi@gmail.com>
2023-10-25 18:34:49 -0700

    defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
-"""Este modulo define la clase BaseModel"""
-
+"""
+Este módulo define la clase BaseModel
+"""
 
 import uuid
-from detetime import datetime
+from datetime import datetime
 import models
 
-
 class BaseModel():
-
-    """Esta clase represent BaseModel"""
+    """
+    Esta es la clase que heredarán las demas clases
+    """
 
     def __init__(self):
-        """metodo que inicialize"""
+        """
+        Este método inicializa los atributos:
+            id, created_at, updated_at
+        """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """
-        devuelve una representacion de nuestra instancia
+        Este metodo retorna una representación de nuestra instancia
         """
 
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, 
+                                     self.id, self.__dict__)
 
     def save(self):
-        """metodo que actualiza la fecha de creación updated_at."""
+        """
+        Este método actualiza la fecha de creación updated_at.
+        """
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        metodo que devuelve un diccionario
-
-        Returns:
-        dict:devuelve un diccionario con los atributos de instancia.
+        Este método retorna un diccionario con los atributos de instancia.
         """
 
         my_dict = dict(self.__dict__)
